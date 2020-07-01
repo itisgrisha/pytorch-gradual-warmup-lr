35677591637003087dfdd9e21505df45ea46a8a7
itisgrisha <itisgrisha@gmail.com>
2020-07-02 02:12:29 +0300

    add ignore iterations

--- warmup_scheduler/scheduler.py
+++ warmup_scheduler/scheduler.py
@@ -13,7 +13,8 @@ class GradualWarmupScheduler(_LRScheduler):
         after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
     """
 
-    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None, ignore_first_iters=0):
+        self.ignore_first_iters = ignore_first_iters
         self.multiplier = multiplier
         if self.multiplier < 1.:
             raise ValueError('multiplier should be greater thant or equal to 1.')
@@ -23,7 +24,7 @@ class GradualWarmupScheduler(_LRScheduler):
         super(GradualWarmupScheduler, self).__init__(optimizer)
 
     def get_lr(self):
-        if self.last_epoch > self.total_epoch:
+        if self.last_epoch - self.ignore_first_iters > self.total_epoch:
             if self.after_scheduler:
                 if not self.finished:
                     self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
@@ -31,10 +32,13 @@ class GradualWarmupScheduler(_LRScheduler):
                 return self.after_scheduler.get_last_lr()
             return [base_lr * self.multiplier for base_lr in self.base_lrs]
 
+        if self.last_epoch < self.ignore_first_iters:
+            return [base_lr * self.multiplier for base_lr in self.base_lrs]
+
         if self.multiplier == 1.0:
-            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+            return [base_lr * (float(self.last_epoch-self.ignore_first_iters) / self.total_epoch) for base_lr in self.base_lrs]
         else:
-            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
+            return [base_lr * ((self.multiplier - 1.) * (self.last_epoch-self.ignore_first_iters) / self.total_epoch + 1.) for base_lr in self.base_lrs]
 
     def step_ReduceLROnPlateau(self, metrics, epoch=None):
         if epoch is None:
